3b0392858fe3a2a969737c4913864842af099550
Shashi Ranjan <shashiranjankv@gmail.com>
2021-11-17 10:31:19 +0530

    Version update

--- iabwrapper/__init__.py
+++ iabwrapper/__init__.py
@@ -1,7 +1,7 @@
 from kivy.logger import Logger
 from kivy.utils import platform
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 _log_message = ("IABwrapper:"
                 + f" {__version__}"
                 + f' (installed at "{__file__}")'
